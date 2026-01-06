3660660fc33eb8c988e42086767db79085574b75
godsom777 <chisomezekielokoro@gmail.com>
2026-01-06 07:58:06 +0100

    feat: enhance font rendering and add Noto Sans for improved typography

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@fontsource/noto-sans/400.css';
+import '@fontsource/noto-sans/600.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
